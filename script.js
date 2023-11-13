f1eb2d372521f3689d2fcaf253ad09d0b66d04da
Chris Ahn <chrisahn@ad.unc.edu>
2023-11-13 18:43:32 -0500

    got the time to show

--- script.js
+++ script.js
@@ -1,23 +1,61 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+// Makes sure the code doesn't run until after the whole webpage is loaded
+$(document).ready(function () {
+  // Interval set to 1 second
+  setInterval(clockTick, 1000)
+  $(".saveBtn").on("click", function() {
+    // Retrieves the value from a sibling element with the class .description
+    var value = $(this).siblings(".description").val();
+    // Retrieves the id attribute of the parent element.
+    var time = $(this).parent().attr("id");
+    // Stores these values in localStorage, using the id as the key and the retrieved value as the data.
+    localStorage.setItem(time, value);
+    // Shows an alert 
+    $(".alert").addClass("show");
+
+    setTimeout(function () {
+
+      $(".alert").removeClass("show");
+
+    }, 5000)
+  })
+
+  setInterval(hourChecker, 10000)
+  // updates UI elements based on the time of day
+  function hourChecker() {
+
+    var currentHour = dayjs().format("HH");
+  
+    $(".time-block").each(function () {
+      var blockHour = $(this).attr("id").split("-")[1];
+      if (currentHour > blockHour) {
+        $(this).addClass("past");
+        $(this).removeClass("present")
+        $(this).removeClass("future")
+      } else if (currentHour == blockHour) {
+        $(this).addClass("present");
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+      } else {
+        $(this).addClass("future");
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+      }
+    })
+  }
+
+  $(".time-block").each(function () {
+    $(this).children(".description").val(localStorage.getItem($(this).attr("id")))
+  })
+  
+  hourChecker();
+
+
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
+
+function clockTick() {
+  $("#currentDay").text(dayjs().tz("America/New_York").format("dddd, MMMM D YYYY, h:mm A"));
+}
+
+
 });
